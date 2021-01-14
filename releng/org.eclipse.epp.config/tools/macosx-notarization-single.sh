9858407e6642d046e0ba9e74a9aaaf226f69cade
Jonah Graham <jonah@kichwacoders.com>
2021-01-13 20:17:31 -0500

    [releng] Prepare repo for 2021-03 M1
    Change-Id: I35e2ba10530a772855d128fa18030d7f6d1fb9ea

--- releng/org.eclipse.epp.config/tools/macosx-notarization-single.sh
+++ releng/org.eclipse.epp.config/tools/macosx-notarization-single.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #*******************************************************************************
-# Copyright (c) 2019, 2020 IBM Corporation and others.
+# Copyright (c) 2019, 2021 IBM Corporation and others.
 #
 # This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License 2.0
--- releng/org.eclipse.epp.config/tools/promote-a-build.sh
+++ releng/org.eclipse.epp.config/tools/promote-a-build.sh
@@ -135,6 +135,7 @@ cat > release.xml <<EOM
 <past>2020-06/R</past>
 <past>2020-09/R</past>
 <present>2020-12/R</present>
+<future>2021-03/M1</future>
 </packages>
 EOM
 
