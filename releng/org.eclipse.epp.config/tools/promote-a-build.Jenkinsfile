942da3baaace5fd748f0c8dc9d082a223b49f45e
Jonah Graham <jonah@kichwacoders.com>
2021-02-25 11:32:52 -0500

    Bug 568117: Update promote-a-build script for new infra
    Change-Id: I914990ab9a6ed8e2e7c15eb5b3bbfbfbf9173fc6

--- releng/org.eclipse.epp.config/tools/promote-a-build.Jenkinsfile
+++ releng/org.eclipse.epp.config/tools/promote-a-build.Jenkinsfile
@@ -1,15 +1,11 @@
 // Based on https://wiki.eclipse.org/Jenkins#Pipeline_job_without_custom_pod_template
 pipeline {
   agent any
-  tools {
-    jdk 'openjdk11-latest'
-  }
   parameters {
     booleanParam(defaultValue: true, description: 'Do a dry run of the build. Everything will be done except the final copy to download which will be echoed', name: 'DRY_RUN')
     string(defaultValue: '2020-12', description: 'The release name. Release is uploaded to https://download.eclipse.org/technology/epp/downloads/release/RELEASE_NAME', name: 'RELEASE_NAME')
     string(defaultValue: 'M1', description: 'The name of the milestone, e.g. M1, M2, M3, RC1 or R', name: 'RELEASE_MILESTONE')
     string(defaultValue: '202009101200', description: 'The name of the directory to place the release, generally the timestamp', name: 'RELEASE_DIR')
-    string(defaultValue: '12345', description: 'The CI build number being promoted from of job simrel.epp-tycho-build', name: 'BUILD_NUMBER')
   }
   options {
     timestamps()
@@ -18,22 +14,13 @@ pipeline {
   stages {
     stage('Upload') {
       steps {
-        // XXX: When EPP is moved to JIRO we need sshagent
+        sshagent ( ['projects-storage.eclipse.org-bot-ssh']) {
          sh './releng/org.eclipse.epp.config/tools/promote-a-build.sh'
+        }
       }
     }
   }
   post {
-    always {
-      // We have a sleep as a post action to keep the
-      // workspace around so it can be examined - very
-      // useful on DRY_RUN builds to examine the
-      // workspace to see if content is right, or to
-      // examine a broken build.
-      // It does mean however that the build needs to
-      // cancelled manually
-      sh 'sleep 1d'
-    }
     cleanup {
       cleanWs()
     }
--- releng/org.eclipse.epp.config/tools/promote-a-build.sh
+++ releng/org.eclipse.epp.config/tools/promote-a-build.sh
@@ -8,123 +8,35 @@ set -x # echo all commands used for debugging purposes
 # RELEASE_NAME=
 # RELEASE_MILESTONE=
 # RELEASE_DIR=
-# BUILD_NUMBER=
-PACKAGES="committers cpp dsl embedcpp java jee modeling parallel php rcp scout"
-PLATFORMS="linux.gtk.aarch64.tar.gz linux.gtk.x86_64.tar.gz macosx.cocoa.x86_64.dmg macosx.cocoa.x86_64.tar.gz win32.win32.x86_64.zip"
-ARCHIVE_URL="https://ci.eclipse.org/packaging/job/simrel.epp-tycho-build/${BUILD_NUMBER}/artifact/org.eclipse.epp.packages/archive/*zip*/archive.zip"
 EPP_DOWNLOADS=/home/data/httpd/download.eclipse.org/technology/epp
 DOWNLOADS=${EPP_DOWNLOADS}/downloads/release/${RELEASE_NAME}/
 REPO=${EPP_DOWNLOADS}/packages/${RELEASE_NAME}/
 
-mkdir downloads
-mkdir p2
-pushd downloads
-
-# ----------------------------------------------------------------------------------------------
-# download the packages from the Jenkins build server
-# rename the packages, i.e. strip the build date, update the package file name, and add the
-# incubation name if required.
-
-echo "wget running quietly - have a look at the workspace to track progress"
-wget --quiet $ARCHIVE_URL
-unzip archive.zip
-pushd archive
-for PACKAGE in $PACKAGES; do
-  for PLATFORM in $PLATFORMS; do
-    NAME=$(echo *_eclipse-${PACKAGE}-${RELEASE_NAME}-${RELEASE_MILESTONE}-${PLATFORM})
-    NEWNAME=`echo ${NAME} | \
-             cut -d "_" -f 2- | \
-             sed 's/linux\.gtk\.aarch64/linux-gtk-aarch64/' | \
-             sed 's/linux\.gtk\.x86\_64/linux-gtk-x86\_64/' | \
-             sed 's/win32\.win32\.x86\_64\./win32\-x86\_64\./' | \
-             sed 's/macosx\.cocoa\.x86\_64/macosx\-cocoa-x86\_64/' | \
-             sed 's/macosx-cocoa-x86_64.dmg/macosx-cocoa-x86_64.dmg-tonotarize/'`
-    # Move and rename file
-    mv ${NAME} ../${NEWNAME}
-  done;
-done
-mv repository ../../p2
-popd
-# archive will be empty now, unless we are only publishing some packages
-rm -rvf archive.zip archive
-
-# place configurations in final location
-for PACKAGE in $PACKAGES; do
-  cp ${WORKSPACE}/packages/org.eclipse.epp.package.${PACKAGE}.feature/epp.website.xml ${PACKAGE}.xml
-  cp ${WORKSPACE}/packages/org.eclipse.epp.package.${PACKAGE}.feature/feature.xml ${PACKAGE}.feature.xml
-  cp ${WORKSPACE}/packages/org.eclipse.epp.package.${PACKAGE}.product/epp.product ${PACKAGE}.product.xml
-done
-
-# Rename incubation packages
-INCUBATION=`ls *.xml | grep -v feature | xargs grep "product name=\"eclipse.*incubation" | sed 's/^.*\(eclipse-.*\)-incubation.*/\1/'`
-echo Found ${INCUBATION} in incubation
-for II in ${INCUBATION}; do
-  echo ".. Renaming ${II} incubating packages"
-  for INCUBATIONPACKAGE in `ls *${II}* | grep -v "incubation"`; do
-    INCUBATIONPACKAGE_FILE=`echo ${INCUBATIONPACKAGE} | sed 's:\(.*\)\('${II}'\)\(.*\):\1\2-incubation\3:'`
-    echo -n ".... Moving ${INCUBATIONPACKAGE} to ${INCUBATIONPACKAGE_FILE}"
-    mv -i ${INCUBATIONPACKAGE} ${INCUBATIONPACKAGE_FILE}
-    echo " done."
-  done
-done
-
-# ----------------------------------------------------------------------------------------------
-# compute the checksum files for each package
-
-for II in eclipse*.zip eclipse*.tar.gz eclipse*.dmg-tonotarize; do
-  echo .. $II
-  md5sum $II >$II.md5
-  sha1sum $II >$II.sha1
-  sha512sum -b $II >$II.sha512
-done
-
-popd # leave downloads
+SSHUSER="genie.packaging@projects-storage.eclipse.org"
+SSH="ssh ${SSHUSER}"
+SCP="scp"
 
 
-# ----------------------------------------------------------------------------------------------
-# Prepare compositeArtifacts.jar/compositeContent.jar
-pushd p2
-if [ "$RELEASE_MILESTONE" != "M1" ] && [ "$RELEASE_MILESTONE" != "R" ]; then
-    # For non M1 build we need to add to the existing p2 content,
-    # for M1 we start from scratch
-
-    # For R build the composite should have just the release in it
-    cp -rp ${REPO}/* .
+ECHO=echo
+if [ "$DRY_RUN" == "false" ]; then
+    ECHO=""
+else
+    echo Dry run of build:
 fi
-mv repository ${RELEASE_DIR}
-cat > addmilestone.xml <<EOM
-<?xml version="1.0" encoding="UTF-8"?>
-<project name="p2 composite repository">
-  <target name="default">
-    <p2.composite.repository>
-      <repository compressed="true" location="." name="${RELEASE_NAME}" />
-      <add>
-        <repository location="${RELEASE_DIR}" />
-      </add>
-    </p2.composite.repository>
-  </target>
-</project>
-EOM
-
-# Use the eclipse we have to build p2 with
-tar xf ../downloads/eclipse-committers-*-linux-gtk-x86_64.tar.gz
-./eclipse/eclipse \
-    -application org.eclipse.ant.core.antRunner \
-    -buildfile addmilestone.xml \
-    default
-rm -rf eclipse
-
-cat > p2.index <<EOM
-version=1
-metadata.repository.factory.order=compositeContent.xml,\!
-artifact.repository.factory.order=compositeArtifacts.xml,\!
-EOM
-
-popd # leave p2
 
 # ----------------------------------------------------------------------------------------------
-# Make the new https://download.eclipse.org/technology/epp/downloads/release/release.xml file
-# The release.xml file is used by Eclipse Webmaster to populate eclipse.org/downloads
+# Move the repo and downloads into their location
+
+$ECHO $SSH mkdir -p ${REPO}
+$ECHO $SSH mv ${EPP_DOWNLOADS}/staging/repository ${REPO}/${RELEASE_DIR}
+if [ "$RELEASE_MILESTONE" != "R" ]; then
+    $ECHO $SSH mv ${EPP_DOWNLOADS}/staging ${DOWNLOADS}/${RELEASE_MILESTONE}
+else
+    $ECHO $SSH mv ${EPP_DOWNLOADS}/staging ${DOWNLOADS}/${RELEASE_DIR}
+fi
+
+# ----------------------------------------------------------------------------------------------
+# Update the release.xml for Web team to consume and create the entries in https://www.eclipse.org/downloads/packages/
 cat > release.xml <<EOM
 <packages>
 <past>2019-03/R</past>
@@ -138,27 +50,61 @@ cat > release.xml <<EOM
 <future>2021-03/M3</future>
 </packages>
 EOM
+$ECHO $SCP release.xml "${SSHUSER}:"${EPP_DOWNLOADS}/downloads/release/release.xml
+
 
 # ----------------------------------------------------------------------------------------------
-# Copy everything to download.eclipse.org
+# Prepare compositeArtifacts.xml/compositeContent.xml that will be made visible with https://ci.eclipse.org/packaging/job/epp-makeVisible/
 
-ECHO=echo
-if [ "$DRY_RUN" == "false" ]; then
-    ECHO=""
+if [ "$RELEASE_MILESTONE" == "R" ]; then
+  UPDATE_SITES=${RELEASE_DIR}
 else
-    echo Dry run of build:
+  UPDATE_SITES=$($SSH "cd ${REPO} && find * -maxdepth 0 -type d | sort -u")
 fi
 
-if [ "$RELEASE_MILESTONE" != "R" ]; then
-    ${ECHO} mkdir -p ${DOWNLOADS}/${RELEASE_MILESTONE}
-    ${ECHO} cp -r downloads/* ${DOWNLOADS}/${RELEASE_MILESTONE}
-else
-    ${ECHO} mkdir -p ${DOWNLOADS}/${RELEASE_DIR}
-    ${ECHO} cp -r downloads/* ${DOWNLOADS}/${RELEASE_DIR}
-fi
-${ECHO} mkdir -p ${REPO}
-${ECHO} cp -r p2/p2.index ${REPO}
-${ECHO} cp -r p2/${RELEASE_DIR} ${REPO}
-${ECHO} cp p2/compositeArtifacts.jar ${REPO}/compositeArtifacts${RELEASE_MILESTONE}.jar
-${ECHO} cp p2/compositeContent.jar ${REPO}/compositeContent${RELEASE_MILESTONE}.jar
-${ECHO} cp release.xml ${EPP_DOWNLOADS}/downloads/release/release.xml
+TIMESTAMP=$(date +%s000)
+NUMBER_UPDATE_SITES=$(echo "$UPDATE_SITES" | wc -w)
+
+CONTENTXML="<?xml version='1.0' encoding='UTF-8'?>
+<?compositeMetadataRepository version='1.0.0'?>
+<repository name='Eclipse Packaging Project EPP ${RELEASE_NAME}'
+    type='org.eclipse.equinox.internal.p2.metadata.repository.CompositeMetadataRepository'
+    version='1.0.0'>
+  <properties size='1'>
+    <property name='p2.timestamp' value='${TIMESTAMP}'/>
+  </properties>
+  <children size='${NUMBER_UPDATE_SITES}'>
+$(
+for site in $UPDATE_SITES
+do
+printf "    <child location='${site}'/>\n"
+done
+)
+  </children>
+</repository>
+"
+
+echo "$CONTENTXML" > ./compositeContent.xml
+$SCP compositeContent.xml "${SSHUSER}:"${REPO}/compositeContent${RELEASE_MILESTONE}.xml
+
+ARTIFACTXML="<?xml version='1.0' encoding='UTF-8'?>
+<?compositeArtifactRepository version='1.0.0'?>
+<repository name='Eclipse Packaging Project EPP ${RELEASE_NAME}'
+    type='org.eclipse.equinox.internal.p2.artifact.repository.CompositeArtifactRepository'
+    version='1.0.0'>
+  <properties size='1'>
+    <property name='p2.timestamp' value='${TIMESTAMP}'/>
+  </properties>
+  <children size='${NUMBER_UPDATE_SITES}'>
+$(
+for site in $UPDATE_SITES
+do
+printf "    <child location='${site}'/>\n"
+done
+)
+  </children>
+</repository>
+"
+
+echo "$ARTIFACTXML" > ./compositeArtifacts.xml
+$SCP compositeArtifacts.xml "${SSHUSER}:"${REPO}/compositeArtifacts${RELEASE_MILESTONE}.xml
