84042998791389bfadf4918ec45c2530ba779a69
Jonah Graham <jonah@kichwacoders.com>
2021-03-03 21:22:47 -0500

    [releng] Prepare repo for 2021-03 RC1
    Change-Id: Ia1f9dcf40e13553b23865ca85b183f77669acea6

--- releng/org.eclipse.epp.config/tools/promote-a-build.sh
+++ releng/org.eclipse.epp.config/tools/promote-a-build.sh
@@ -47,7 +47,7 @@ cat > release.xml <<EOM
 <past>2020-06/R</past>
 <past>2020-09/R</past>
 <present>2020-12/R</present>
-<future>2021-03/M3</future>
+<future>2021-03/RC1</future>
 </packages>
 EOM
 $ECHO $SCP release.xml "${SSHUSER}:"${EPP_DOWNLOADS}/downloads/release/release.xml
