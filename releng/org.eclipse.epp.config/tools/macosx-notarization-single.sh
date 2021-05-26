6e716ec38909e18b9b1e09faecac249ce295dd42
Jonah Graham <jonah@kichwacoders.com>
2021-05-26 16:12:06 -0400

    Bug 571669: Don't request status if initial upload fails
    If we get a 502/503 error on the initial upload, retry the upload,
    don't run pointless status checks with no UUID first.
    
    Change-Id: I7732a2b0f9a7635cf251464058f13c50d0321bfe

--- releng/org.eclipse.epp.config/tools/macosx-notarization-single.sh
+++ releng/org.eclipse.epp.config/tools/macosx-notarization-single.sh
@@ -25,7 +25,7 @@ cp "${DMG_FILE}"-tonotarize "${DMG}"
 
 PRIMARY_BUNDLE_ID="$(echo ${DMG} | sed  's/-macosx.cocoa.x86_64.dmg//g' | sed -E 's/^[0-9\-]*_(.*)/\1/g')"
 
-retryCount=3
+retryCount=5
 while [ ${retryCount} -gt 0 ]; do
 
   RESPONSE_RAW=$(curl  --write-out "\n%{http_code}" -s -X POST -F file=@${DMG} -F 'options={"primaryBundleId": "'${PRIMARY_BUNDLE_ID}'", "staple": true};type=application/json' https://cbi.eclipse.org/macos/xcrun/notarize)
@@ -34,13 +34,17 @@ while [ ${retryCount} -gt 0 ]; do
   UUID="$(echo "${RESPONSE}" | jq -r '.uuid')"
   STATUS="$(echo "${RESPONSE}" | jq -r '.notarizationStatus.status')"
 
-  while [[ ${STATUS} == 'IN_PROGRESS' || ${STATUS_CODE} == '503' || ${STATUS_CODE} == '502' ]]; do
-    sleep 1m
-    RESPONSE_RAW=$(curl  --write-out "\n%{http_code}" -s https://cbi.eclipse.org/macos/xcrun/${UUID}/status)
-    RESPONSE=$(head -n1 <<<"${RESPONSE_RAW}")
-    STATUS_CODE=$(tail -n1 <<<"${RESPONSE_RAW}")
-    STATUS=$(echo ${RESPONSE} | jq -r '.notarizationStatus.status')
-  done
+  if [[ ${STATUS_CODE} == '503' || ${STATUS_CODE} == '502' ]]; then
+    echo Initial upload failed, Retrying
+  else
+    while [[ ${STATUS} == 'IN_PROGRESS' || ${STATUS_CODE} == '503' || ${STATUS_CODE} == '502' ]]; do
+      sleep 1m
+      RESPONSE_RAW=$(curl  --write-out "\n%{http_code}" -s https://cbi.eclipse.org/macos/xcrun/${UUID}/status)
+      RESPONSE=$(head -n1 <<<"${RESPONSE_RAW}")
+      STATUS_CODE=$(tail -n1 <<<"${RESPONSE_RAW}")
+      STATUS=$(echo ${RESPONSE} | jq -r '.notarizationStatus.status')
+    done
+  fi
 
   if [[ ${STATUS} != 'COMPLETE' ]]; then
     echo "Notarization failed: ${RESPONSE}"
