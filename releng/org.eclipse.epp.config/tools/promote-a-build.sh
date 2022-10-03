32e8a30d628d9ac8ab45c6a9c7d436bbc745518c
Jonah Graham <jonah@kichwacoders.com>
2022-10-03 10:38:49 -0400

    [releng] Prepare repo for 2022-12 M1

--- releng/org.eclipse.epp.config/tools/promote-a-build.sh
+++ releng/org.eclipse.epp.config/tools/promote-a-build.sh
@@ -73,6 +73,7 @@ cat > release.xml <<EOM
 <past>2022-03/R</past>
 <past>2022-06/R</past>
 <present>2022-09/R</present>
+<future>${RELEASE_NAME}/${RELEASE_MILESTONE}</future>
 </packages>
 EOM
 $ECHO $SCP release.xml "${SSHUSER}:"${EPP_DOWNLOADS}/downloads/release/release.xml
