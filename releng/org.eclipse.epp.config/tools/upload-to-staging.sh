12380d418eb53e03971c4df8bef5b1664d69b791
Jonah Graham <jonah@kichwacoders.com>
2023-02-23 10:05:44 -0500

    [releng] Prepare repo for 2023-03 M3
    Change-Id: I991f64642872daf5291c7dce25e6cb41de51e2ff

--- releng/org.eclipse.epp.config/tools/upload-to-staging.sh
+++ releng/org.eclipse.epp.config/tools/upload-to-staging.sh
@@ -96,11 +96,11 @@ Last +1 received for each package and platform (apologies if I missed one of you
 
 Packages:
 committers - 2022-12 RC1
-cpp - 2022-12 RC1
-dsl - 2022-12 RC1
+cpp - 2023-03 M2
+dsl - 2023-03 M2
 embedcpp - 2022-12 RC1
-java - 2022-09 M2
-jee - 2022-12 RC1
+java - 2023-03 M2
+jee - 2023-03 M2
 modeling - 2022-12 M3
 parallel - 2022-03 RC2 (tested by Ed in 2022-09 RC1)
 php - 2020-12 RC2 (tested by Ed in 2022-09 RC1)
@@ -108,11 +108,11 @@ rcp - 2022-09 RC2
 scout - 2023-03 M1
 
 Platforms:
-Linux x86_64 - 2022-12 RC1
+Linux x86_64 - 2023-03 M2
 Linux aarch64 - 2022-12 RC1
-Windows - 2022-09 RC2
+Windows - 2023-03 M2
 macOS x86_64 - 2022-09 M3
-macOS aarch64 - 2022-12 RC1
+macOS aarch64 - 2023-03 M2
 
 Thank you for testing!
 
