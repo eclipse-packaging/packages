d45c6fee7f21c017b49342f956562ba4d3281575
Jonah Graham <jonah@kichwacoders.com>
2023-10-05 13:16:44 -0400

    Updated Last +1 up until 2023-09 RC2

--- releng/org.eclipse.epp.config/tools/upload-to-staging.sh
+++ releng/org.eclipse.epp.config/tools/upload-to-staging.sh
@@ -95,23 +95,23 @@ Last +1 received for each package and platform (apologies if I missed one of you
 
 Packages:
 committers - 2023-06 RC2
-cpp - 2023-06 RC2
-dsl - 2023-06 RC2
-embedcpp - 2023-06 RC2
-java - 2023-06 RC2
-jee - 2023-06 RC2
-modeling - 2023-09 M2
+cpp - 2023-09 RC2
+dsl - 2023-09 RC2
+embedcpp - 2023-09 RC2
+java - 2023-09 RC2
+jee - 2023-09 RC2
+modeling - 2023-09 RC1
 parallel - 2022-03 RC2 (tested by Ed in 2023-03 RC2)
 php - 2023-06 RC2
-rcp - 2023-06 RC2
+rcp - 2023-09 RC2
 scout - 2023-06 RC2
 
 Platforms:
-Linux x86_64 - 2023-09 M2
-Linux aarch64 - 2023-06 RC2
-Windows - 2023-06 RC2
-macOS x86_64 - 2023-06 RC2
-macOS aarch64 - 2023-06 RC2
+Linux x86_64 - 2023-09 RC2
+Linux aarch64 - 2023-09 RC2
+Windows - 2023-09 RC2
+macOS x86_64 - 2023-09 RC2
+macOS aarch64 - 2023-09 RC2
 
 Thank you for testing!
 
