994900e065f327aef847b03197476cadba9e35bc
Jonah Graham <jonah@kichwacoders.com>
2025-02-27 14:12:30 -0500

    Prepare for 2025-03 RC2

--- releng/org.eclipse.epp.config/tools/upload-to-staging.sh
+++ releng/org.eclipse.epp.config/tools/upload-to-staging.sh
@@ -55,7 +55,7 @@ Last +1 received for each package and platform (apologies if I missed one of you
 
 Packages:
 committers - 2024-12 RC2
-cpp - 2025-03 M1
+cpp - 2025-03 RC1
 dsl - 2025-03 M1
 embedcpp - 2024-09 RC2
 java - 2024-12 RC2
@@ -66,9 +66,9 @@ rcp - 2024-12 RC2
 scout - 2024-09 RC2
 
 Platforms:
-Linux x86_64 - 2025-03 M1
+Linux x86_64 - 2025-03 RC1
 Linux aarch64 - 2023-09 RC2
-Linux riscv64 - 2024-12 RC2
+Linux riscv64 - 2025-03 M3
 Windows x86_64 - 2024-12 RC2
 Windows on Arm - 2024-12 RC1
 macOS x86_64 - 2024-09 RC2
