db241d5be0a3d1cc792ddddc7e1558900f884adb
Jonah Graham <jonah@kichwacoders.com>
2025-07-10 19:47:12 -0400

    Update last recorded +1s

--- releng/org.eclipse.epp.config/tools/upload-to-staging.sh
+++ releng/org.eclipse.epp.config/tools/upload-to-staging.sh
@@ -55,22 +55,22 @@ Last +1 received for each package and platform (apologies if I missed one of you
 
 Packages:
 committers - 2024-12 RC2
-cpp - 2025-06 RC1
+cpp - 2025-06 RC2
 dsl - 2025-06 M2
 embedcpp - 2024-09 RC2
 java - 2025-06 RC1
-jee -  2025-03 RC2
+jee -  2025-06 RC2
 modeling - 2025-06 RC1
 php - 2025-06 M1
-rcp - 2024-12 RC2
+rcp - 2025-06 RC2
 scout - 2025-03 RC2
 
 Platforms:
-Linux x86_64 - 2025-06 RC1
+Linux x86_64 - 2025-06 RC2
 Linux aarch64 - 2023-09 RC2
-Linux riscv64 - 2025-06 M2
+Linux riscv64 - 2025-06 RC2
 Windows x86_64 - 2025-06 RC1
-Windows on Arm - 2025-03 RC2
+Windows on Arm - 2025-06 RC2
 macOS x86_64 - 2024-09 RC2
 macOS aarch64 - 2025-06 M2
 
